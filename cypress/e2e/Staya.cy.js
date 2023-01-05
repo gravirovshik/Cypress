1d93950e92e71d9a2f98ec9ce162bcda02805669
Roman Belov <gravirovshik@yandex.ru>
2023-01-05 18:24:15 +0300

    add Staya

---
+++ cypress/e2e/Staya.cy.js
@@ -0,0 +1,28 @@
+describe('Staya Home Work', () => {
+
+    it('Автотесты для формы логина и пароля на Staya', () => {
+
+      cy.visit('https://staya.dog/login')
+      
+      cy.get("form[action='#']>input[placeholder='E-mail*']").type("gravirovshik@yandex.ru")
+      cy.get("form[action='#']>input[placeholder='Пароль*']").type("iLoveqastudio1")
+      cy.get("button[type='submit']>span[class='s-button__content']").click()
+      
+      cy.get(".profile-orders__title.profile-orders__title_desktop").should('contain', 'Мои заказы')
+
+     // cy.get("#messageHeader").should('contain', 'Успешно отправили пароль на e-mail')
+      //cy.get("button[id='exitMessageButton']>img[alt='exit icon']").should("be.visible");
+  
+    })
+  
+  
+
+  
+  
+  
+    
+  
+  
+  
+  
+  })
