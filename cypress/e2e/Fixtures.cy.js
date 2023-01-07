f0bdf7b07ef4fe1b4d3e77b2943aff6128881f5a
Roman Belov <gravirovshik@yandex.ru>
2023-01-07 17:41:39 +0300

    finish fixtures

--- cypress/e2e/Fixtures.cy.js
+++ cypress/e2e/Fixtures.cy.js
@@ -1,8 +1,47 @@
 describe('Fixtures', () => {
 
-    it('Direct', () => {
-      cy.visit('https://opensource-demo.orangehrmlive.com/')
-      cy.title().should('eq', 'OrangeHRM')
+    it.skip('Using correct data file', () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+
+        cy.fixture('orange').then((data) => {
+
+            cy.get("input[placeholder='Username']").type(data.username)
+            cy.get("input[placeholder='Password']").type(data.password)
+            cy.get("button[type='submit']").click()
+            cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('contain', data.expected);
+
+        })
+    })
+
+    
+    it('Using multple data file', () => {
+
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')       
+
+        cy.fixture('multiorange').then((data) => {
+            data.forEach((userdata)=>{
+
+            cy.get("input[placeholder='Username']").type(userdata.username)
+            cy.get("input[placeholder='Password']").type(userdata.password)
+            cy.get("button[type='submit']").click()
+
+            if(userdata.username == "Admin" && userdata.password == "admin123"){
+
+                cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').should('contain', userdata.expected);
+                cy.get('.oxd-userdropdown-tab > .oxd-icon').click();
+                cy.get(':nth-child(4) > .oxd-userdropdown-link').click();
+
+            }
+
+            else {
+
+                cy.get('.oxd-alert-content > .oxd-text').should('contain', userdata.expected);
+
+            }
+            
+
+            });
+        })
     })
   
   })
